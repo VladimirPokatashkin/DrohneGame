7290014429b1fe1fa9883e9581dda1d93f579b6e
vovadener <vovanpokatashkin548@gmail.com>
2026-05-23 01:04:22 +0300

    remove drohne single command

--- src/main/java/lang/syntaxtree/both/DrohneCommandNode.java
+++
@@ -1,6 +0,0 @@
-package lang.syntaxtree.both;
-
-import lang.syntaxtree.expression.Expression;
-import lang.syntaxtree.statement.Statement;
-
-public interface DrohneCommandNode extends Expression, Statement {}
