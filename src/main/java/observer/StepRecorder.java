6d41d32071c2e051fe866d329cf1c44f9691ed48
vovadener <vovanpokatashkin548@gmail.com>
2026-07-31 10:42:11 +0300

    add observers for step recording

---
+++ src/main/java/observer/StepRecorder.java
@@ -0,0 +1,35 @@
+package observer;
+
+import dto.StepDTO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor
+public class StepRecorder implements DrohneObserver {
+	private final List<StepDTO> steps = new ArrayList<>();
+
+	@Override
+	public void start(int x, int y, int z) {
+		steps.add(new StepDTO(x, y, z, "start", Map.of()));
+	}
+
+	@Override
+	public void moved(int x, int y, int z) {
+		steps.add(new StepDTO(x, y, z, "move", Map.of()));
+	}
+
+	@Override
+	public void crashed(int x, int y, int z) {
+		steps.add(new StepDTO(x, y, z, "crash", Map.of()));
+	}
+
+	@Override
+	public void scanned(int x, int y, int z, String direction) {
+		steps.add(new StepDTO(x, y, z, "scan", Map.of("direction", direction)));
+	}
+}
